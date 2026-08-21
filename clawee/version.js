847e81243272394f10df223c8e09081fe3351ee9
Clawee <290954373+clawee-git@users.noreply.github.com>
2026-08-21 00:23:38 -0700

    [RELEASED: clawee] 2026-08-21 v0.2.12.2026.08.21.c995a532

--- clawee/version.js
+++ clawee/version.js
@@ -1,1 +1,1 @@
-__claweeVersion({"component":"clawee","version":"0.2.11","stamp":"v0.2.11.2026.08.21.c2f29f01"});
+__claweeVersion({"component":"clawee","version":"0.2.12","stamp":"v0.2.12.2026.08.21.c995a532"});
