557f2d11f96cc6e4020b4f0e19e5a42a30ed5f00
Clawee <290954373+clawee-git@users.noreply.github.com>
2026-08-26 17:32:21 -0700

    [RELEASED: claweed] 2026-08-27 v0.2.14.2026.08.27.3d3078fe

--- claweed/version.js
+++ claweed/version.js
@@ -1,1 +1,1 @@
-__claweeVersion({"component":"claweed","version":"0.2.13","stamp":"v0.2.13.2026.08.26.0025ffab"});
+__claweeVersion({"component":"claweed","version":"0.2.14","stamp":"v0.2.14.2026.08.27.3d3078fe"});
